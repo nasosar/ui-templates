34d020fe4ad6ccc59ec6d18ea8e16400cefbc48a
Natasha <natashasosar@gmail.com>
2023-02-16 21:51:12 -0300

    Build second carousel second attempt

--- carousel-slider/javascript/script.js
+++ carousel-slider/javascript/script.js
@@ -18,3 +18,39 @@ function carousel1() {
 setInterval(carousel1, 1800);
 
 //SECOND CAROUSEL: CLICK TO SLIDE
+
+const controls = document.querySelectorAll('.control');
+
+let currentItem = 0;
+const items = document.querySelectorAll('.img-crousel2');
+const maxItems = items.length;
+
+controls.forEach(control => {
+    control.addEventListener('click', () => {
+
+        const isLeft = control.classList.contains('arrow-left');
+
+        if (isLeft) {
+            currentItem -= 1;
+        } else {
+            currentItem += 1;
+        }
+
+        if (currentItem >= maxItems) {
+            currentItem = 0;
+        }
+
+        if (currentItem < 0) {
+            currentItem = maxItems -1;
+        }
+
+        items.forEach(item =>
+            item.classList.remove('current-img'));
+
+        items[currentItem].scrollIntoView({inline: "center", behavior: "smooth", block: "nearest"});
+
+        items[currentItem].classList.add('current-img');
+
+        console.log ('control', isLeft, currentItem);
+    });
+});
