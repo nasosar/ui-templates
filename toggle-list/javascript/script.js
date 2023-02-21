877ff853380357b58c6bf5bf4be3ebf105707bdd
Natasha Rebelo <natashasosar@gmail.com>
2023-02-20 22:17:43 -0300

    Reorganize FAQ html and simplify javascript logic

--- toggle-list/javascript/script.js
+++ toggle-list/javascript/script.js
@@ -1,20 +1,7 @@
-let controls = document.querySelector('.titlelist');
-let arrowDown = document.querySelector('#downarrow');
-let arrowUp = document.querySelector('#uparrow');
-const item1 = document.querySelector('#item1');
+const faqs = document.querySelectorAll(".faq-item");
 
-// controls.forEach (control => {
-//     control.addEventListener( 'click', () => {
-//         console.log('i work')
-//         toggleitems.classList.toggle('.activeitem');
-//     })
-// })
-
-    
-
-function classToggle() {
-    controls.classList.toggle("titlelist-active");
-    item1.classList.toggle("activeitem");
-    arrowDown.classList.toggle("ocultitem");
-    arrowUp.classList.toggle("activeitem");
-}
+faqs.forEach(faq => {
+    faq.addEventListener('click', () => {
+        faq.classList.toggle('active');
+    })
+})
