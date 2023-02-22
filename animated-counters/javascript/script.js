16d26ab8c079387974247fa3466cad02507f267a
Natasha Rebelo <natashasosar@gmail.com>
2023-02-22 10:42:29 -0300

    Adjust scrip to trigger counters when they are scrolled into view

---
+++ animated-counters/javascript/script.js
@@ -0,0 +1,55 @@
+let counters = document.querySelectorAll('.counter');
+let countInterval = 4000;
+let counterBox = document.querySelector('.listbox')
+
+
+function isInViewport(counterBox) {
+    // Get the bounding client rectangle position in the viewport
+    let viewCheck = counterBox.getBoundingClientRect();
+    
+    // Checking part. Here the code checks if it's *fully* visible
+    // Edit this part if you just want a partial visibility
+    if (
+        viewCheck.top >= 0 &&
+        viewCheck.left >= 0 &&
+        viewCheck.right <= (window.innerWidth || document.documentElement.clientWidth) &&
+        viewCheck.bottom <= (window.innerHeight || document.documentElement.clientHeight)
+    ) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+window.addEventListener('scroll', function () {
+    if (isInViewport(counterBox)) {
+        counters.forEach(counter => {
+            let startValue = 0;
+            let endValue = parseInt(counter.getAttribute('data-val'));
+            let duration = Math.floor(countInterval / endValue);
+        
+            let triggerCount = setInterval (function () {
+                startValue += 1;
+                counter.textContent = startValue;
+                if (startValue == endValue) {
+                    clearInterval(triggerCount);
+                }
+            }, duration);
+        });
+    }
+}, {once : true});
+
+
+// counters.forEach(counter => {
+//     let startValue = 0;
+//     let endValue = parseInt(counter.getAttribute('data-val'));
+//     let duration = Math.floor(countInterval / endValue);
+
+//     let triggerCount = setInterval (function () {
+//         startValue += 1;
+//         counter.textContent = startValue;
+//         if (startValue == endValue) {
+//             clearInterval(triggerCount);
+//         }
+//     }, duration);
+// });
